8d99dd2db7bdcb0382c37fb54fd784beec206f1c
Akash Bhattacharyya <72349798+AkashBhattacharyya99@users.noreply.github.com>
2021-11-24 18:58:27 +0530

    Add files via upload

---
+++ assets/db.php
@@ -0,0 +1,11 @@
+<?php 
+    $con = new mysqli('localhost:3307','root','','mybank');
+    define('bankName', 'Bank Of Kolkata',true);
+    $ar = $con->query("select * from userAccounts,branch where id = '$_SESSION[userId]' AND userAccounts.branch = branch.branchId");
+    $userData = $ar->fetch_assoc();
+?>
+<script type="text/javascript">
+	$(function () {
+  $('[data-toggle="tooltip"]').tooltip()
+})
+</script>
